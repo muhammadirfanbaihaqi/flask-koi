8543823632d43b33daf09775a6546ed66be5a5c6
muhammadirfanbaihaqi <muhammadirfanbaihaqi538@gmail.com>
2025-05-02 15:57:50 +0700

    up

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from urllib.parse import quote as url_quote
+
 # from dotenv import load_dotenv
 
 # import os
