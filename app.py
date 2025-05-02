7e06b044cb2beaf8d1b6e118acd9d402f0e00434
muhammadirfanbaihaqi <muhammadirfanbaihaqi538@gmail.com>
2025-05-02 16:45:54 +0700

    HAPUS KODE

--- app.py
+++ app.py
@@ -22,67 +22,12 @@ collection = db["sensor"]
 data_terakhir = {}
 jadwal_pakan = [[7, 0], [12, 0], [18, 0]]  # Default jadwal pakan
 
-# ===================== INISIALISASI MODEL ====================
-# from torch.serialization import add_safe_globals
-# import ultralytics.nn.tasks as tasks
-# from ultralytics import YOLO
-
-# # Izinkan class model dari Ultralytics saat load
-# add_safe_globals([tasks.DetectionModel])
-
-# Load model YOLOv8
-# model = YOLO("models/best.pt")  # Ganti nama file jika perlu
-
 # ===================== ROUTING UTAMA ========================
 @app.route('/')
 def home():
     tz = pytz.timezone('Asia/Jakarta')
     current_time = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
     return f"✅ API AIoT Aktif! : {current_time}"
-
-    
-
-# # ===================== DETEKSI IKAN =========================
-# @app.route('/detect', methods=['POST'])
-# def detect_fish():
-#     try:
-#         file = request.files['image']
-#         image = Image.open(file).convert("RGB")
-
-#         img_array = np.array(image)
-#         img_bgr = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
-
-#         results = model(img_bgr)
-#         num_fish = len(results[0].boxes)
-
-#         result_img = results[0].plot()
-#         _, buffer = cv2.imencode(".jpg", result_img)
-#         result_img_bytes = buffer.tobytes()
-
-#         return jsonify({
-#             "num_fish": num_fish,
-#             "image": result_img_bytes.hex()
-#         })
-
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-# ===================== ENDPOINT SENSOR ======================
-# @app.route('/sensor', methods=['POST'])
-# def simpan_data():
-#     data = request.get_json()
-#     if not data:
-#         return jsonify({"error": "Tidak ada data yang dikirim"}), 400
-
-    # data['timestamp'] = datetime.utcnow()
-    # collection.insert_one(data)
-#     return jsonify({"message": "Data berhasil disimpan"}), 201
-
-# @app.route('/sensor', methods=['GET'])
-# def ambil_data_terbaru():
-#     data = collection.find().sort("timestamp", -1).limit(1)
-#     return dumps(data[0]), 200
-
 
 # ================= ENDPOINT SENSOR ================
 @app.route('/sensor', methods=['POST'])
@@ -119,11 +64,6 @@ def ambil_data():
 #     collection.insert_one(data_terakhir)
 #     print("📥 Data Diterima:", data_terakhir)
 #     return jsonify({"message": "Data berhasil disimpan"}), 201
-
-
-# @app.route('/sensor', methods=['GET'])
-# def ambil_data():
-#     return jsonify(data_terakhir), 200
 
 @app.route('/sensor/history', methods=['GET'])
 def ambil_riwayat_data():
